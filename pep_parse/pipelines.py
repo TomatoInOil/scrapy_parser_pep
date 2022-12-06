84455beb0eeb189527a41f0b22f1ca935d145581
Даниил Паутов <danpautoff@yandex.ru>
2022-12-06 10:01:10 +0300

    Рефакторинг pipelines.

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,6 +1,5 @@
 import csv
-from datetime import datetime as dt
-from datetime import timezone
+from datetime import datetime, timezone
 from pathlib import Path
 
 STATUS_COL = "Статус"
@@ -14,7 +13,7 @@ class PepParsePipeline:
 
     def open_spider(self, spider):
         self.time = (
-            dt.now(timezone.utc)
+            datetime.now(timezone.utc)
             .isoformat(timespec="seconds")[:-6]
             .replace(":", "-")
         )
@@ -27,10 +26,9 @@ class PepParsePipeline:
         self.total = 0
 
     def process_item(self, item, spider):
-        if item["status"] in self.summary_data:
-            self.summary_data[item["status"]] += 1
-        else:
-            self.summary_data[item["status"]] = 1
+        self.summary_data[item["status"]] = (
+            self.summary_data.get(item["status"], 0) + 1
+        )
         self.total += 1
         return item
 
@@ -38,8 +36,8 @@ class PepParsePipeline:
         with open(
             BASE_DIR / f"status_summary_{self.time}.csv", "a", newline=""
         ) as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=STATUS_SUM_FIELDS)
-            for status, number in sorted(self.summary_data.items()):
-                writer.writerow({STATUS_COL: status, SUMMARY_COL: number})
-            writer.writerow({STATUS_COL: "Total", SUMMARY_COL: self.total})
+            writer = csv.writer(csvfile)
+            rows = sorted(self.summary_data.items())
+            rows.append(("Total", self.total))
+            writer.writerows(rows)
         spider.close()
